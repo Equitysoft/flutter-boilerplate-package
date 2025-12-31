dcd857f0d0e415d2d1de8629204b2370dd31f9d1
Harsh Chudasma <127497179+EquitysoftHarshChudasma@users.noreply.github.com>
2025-12-31 16:30:13 +0530

    - Sepration as proper field wise and better

--- android/app/src/main/kotlin/com/boilerplate/app_boilerplate/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.boilerplate.app_boilerplate
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/services/services.dart
+++
@@ -1,5 +0,0 @@
-// Services Exports
-// Import this file to access all services
-
-export 'prefs_service.dart';
-export 'firebase_service.dart';
